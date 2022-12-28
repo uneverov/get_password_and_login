3869fbbd2201b3275aa74e0ad5f1834c2aa6c81c
uneverov <uneverov@gmail.com>
2022-12-29 02:08:58 +1000

    made minimize window to tray

--- settings.py
+++ settings.py
@@ -1,12 +1,4 @@
-BLACK = '#141414'
-WHITE = '#fff'
-ORANGE = '#39d4b0'
-VIOLET = '#7010e6'
-BLUE = '#79B0EE'
-SAND = '#D6C5B6'
-GREEN = '#74CD67'
-RED = '#FF585D'
-LILAC = '#c8a2c8'
+
 COLORS = {'ORANGE': '#39d4b0', 'VIOLET': '#7010e6', 'BLUE': '#79B0EE',
           'SAND': '#D6C5B6', 'GREEN': '#74CD67', 'RED': '#FF585D',
           'LILAC': '#c8a2c8', 'BLACK': '#141414', 'WHITE': '#fff'}
