d1f30fcf56e4d577505a706aab7ae084e1e0ab91
uneverov <uneverov@gmail.com>
2023-01-30 13:21:37 +1000

    now our chat able to connect to server!

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+import threading
+
 import keyboard
 from Objects.Button import onkeypress, place_buttons
 from Objects.Gpals import Gpals
 from Objects.MenuButton import MenuBar
 from Objects.Windows import MainWindow
 
-window = MainWindow()
 
-gpals = Gpals()
-menu_bar = MenuBar(window, gpals)
-place_buttons(gpals=gpals.gpals, window=window)
-keyboard.on_press(onkeypress)
-window.mainloop()
+def app():
+    window = MainWindow()
+    gpals = Gpals()
+    MenuBar(window, gpals)
+    place_buttons(gpals=gpals.gpals, window=window)
+    keyboard.on_press(onkeypress)
+    window.mainloop()
+
+app = threading.Thread(target=app)
+app.start()
