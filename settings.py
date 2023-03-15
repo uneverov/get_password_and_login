c083d894e5f997bed4a9c5cfebc7520c283c03b1
uneverov <uneverov@gmail.com>
2023-03-15 17:27:49 +1000

    remove server.py fix small bugs

--- settings.py
+++ settings.py
@@ -4,3 +4,6 @@ pyglet.font.add_file('Hack-Bold.ttf')
 COLORS = {'ORANGE': '#39d4b0', 'VIOLET': '#7010e6', 'BLUE': '#79B0EE',
           'SAND': '#D6C5B6', 'GREEN': '#74CD67', 'RED': '#FF585D',
           'LILAC': '#c8a2c8', 'BLACK': '#141414', 'WHITE': '#fff'}
+
+host_name = "194.58.107.248"
+server_port = 20
